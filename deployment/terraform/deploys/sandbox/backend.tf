baf421d05927c495cf2f22ff77c0f8ecfe22fe0d
Punam-Dev <punam.pal.dev@gmail.com>
2022-12-03 23:56:18 +0530

    fix s3 bucket name

--- deployment/terraform/deploys/sandbox/backend.tf
+++ deployment/terraform/deploys/sandbox/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "datadog-logging--sandbox"
+    bucket = "datadog-logging-sandbox"
     key = "datadog-logging-sandbox.tfstate"
     region = "ap-south-1"
     role_arn = "arn:aws:iam::579253604158:role/terraform-user-deployment-role"
